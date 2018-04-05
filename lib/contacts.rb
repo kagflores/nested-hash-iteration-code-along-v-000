83dcfa3c10cf37175bdd891713a75076b7d89d47
Karen Flores <karen.ag.flores@gmail.com>
2018-04-05 13:01:26 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,7 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, data_hash|
+    binding.pry
     data.each do |attribute, value|
       if attribute == :favorite_ice_cream_flavors
         :favorite_ice_cream_flavors.delete("strawberry")
